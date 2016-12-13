1797cb29890a47174bda3b44e8b880e23c948f06
KRdimov <milush_karadimov@yahoo.com>
2016-12-13 13:13:10 +0200

    Removing unnecessary files

--- zombies/Bow.java
+++
@@ -1,9 +0,0 @@
-package zombies;
-
-public class Bow extends Weapon {
-
-	public Bow(String name, int damage) {
-		super(name, damage, 10, 2);
-	}
-
-}
--- zombies/Sword.java
+++
@@ -1,9 +0,0 @@
-package zombies;
-
-public class Sword extends Weapon{
-
-	public Sword(String name, int damage) {
-		super(name, damage, 20, 1);
-	}
-
-}
