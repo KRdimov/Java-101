d9d9e474c58454f6e3c16a1cf1085bd80fb6af3a
KRdimov <milush_karadimov@yahoo.com>
2017-01-02 17:20:15 +0200

    Adding unfinished class

--- week05/MyOwnMekiciSolver.java
+++ week05/MyOwnMekiciSolver.java
@@ -1,32 +1,22 @@
 package week05;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class MyOwnMekiciSolver {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		int inputLines = sc.nextInt();
+		
 		int mekiciCount = sc.nextInt();
 		int kotloniCount = sc.nextInt();
-		Map<Integer, Integer> cookTimeMekici = new TreeMap<>();
 		
 		for (int i = 0; i < kotloniCount; i++) {
-			cookTimeMekici.put(sc.nextInt(), 0);
+			Integer cookTime = sc.nextInt();
 		}
 		
-//		while (mekiciCount > 0) {
-//			for(Map.Entry<Integer, Integer> cookTimeMekica : cookTimeMekici.entrySet()) {
-//				if(mekiciCount > 0) {
-//					cookTimeMekica.setValue(cookTimeMekica.getValue() + 1);
-//					mekiciCount--;
-//					continue;
-//				}
-//				break;
-//			}
-//		}
+		//TODO
 	}
 }
