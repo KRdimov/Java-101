6168aa2564a17f0168c0f0b592aa1055785bd667
KRdimov <milush_karadimov@yahoo.com>
2017-02-14 20:46:49 +0200

    Adding week16 JSON tasks

---
+++ week16/URLReader.java
@@ -0,0 +1,29 @@
+package week16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class URLReader {
+	private String url;
+	
+	public URLReader(String city) {
+		url = "http://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=9ed81d9300f326bbd3f1ef06bb0f1207";
+	}
+	
+	public String getResponse() throws Exception {	
+		URL weatherUrl = new URL(url);
+		BufferedReader in = new BufferedReader(new InputStreamReader(weatherUrl.openStream()));
+		return bufferedReaderToString(in);
+	}
+
+	private static String bufferedReaderToString(BufferedReader in) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		while ((line = in.readLine()) != null) {
+			sb.append(line);
+		}
+		return sb.toString();
+	}
+}
---
+++ week16/WeatherApp.java
@@ -0,0 +1,28 @@
+package week16;
+
+import java.util.Scanner;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+public class WeatherApp {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		URLReader urlReader = new URLReader(sc.nextLine()); 
+		String weatherAppInfo = urlReader.getResponse();
+		System.out.println(getInfo(weatherAppInfo));
+	}
+
+	private static String getInfo(String weatherAppInfo) {
+		String output = "";
+		JsonObject jsonInfo = new JsonParser().parse(weatherAppInfo).getAsJsonObject();
+		JsonObject jsonInfoSys = new JsonParser().parse(jsonInfo.get("sys").toString()).getAsJsonObject();
+		JsonObject jsonInfoMain = new JsonParser().parse(jsonInfo.get("main").toString()).getAsJsonObject();
+		output += "Place: " + jsonInfoSys.get("country") + ", " + jsonInfo.get("name") + "\n";
+		output += "Temperature: " + jsonInfoMain.get("temp") + ", Pressure: " + jsonInfoMain.get("pressure") + ", Humidity: " + jsonInfoMain.get("humidity") + "\n";
+		
+		return output;
+	}
+}
